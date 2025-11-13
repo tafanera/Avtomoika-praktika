129050b1d10f144178bf3f10556d346c78f857b9
tafanera <ilyastepichev2007@gmail.com>
2025-11-13 22:23:26 +0300

    игнор .idea, bin, obj, Models

--- WebApp.Infrastructure/Contexts/ApplicationContext.cs
+++ WebApp.Infrastructure/Contexts/ApplicationContext.cs
@@ -18,13 +18,30 @@ public class ApplicationContext : DbContext
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Car>()
+            .HasOne(c => c.User)
+            .WithMany(u => u.Cars)
+            .HasForeignKey(c => c.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
         modelBuilder.Entity<Order>()
             .HasOne(o => o.User)
             .WithMany(u => u.Order)
             .HasForeignKey(o => o.UserId)
             .OnDelete(DeleteBehavior.Restrict);
 
-        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Order>()
+            .HasOne(o => o.Cars)
+            .WithMany(c => c.Order)
+            .HasForeignKey(o => o.CarId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Order>()
+            .HasMany(o => o.Services)
+            .WithMany(s => s.Orders)
+            .UsingEntity(j => j.ToTable("OrderServices"));
     }
     
 }
